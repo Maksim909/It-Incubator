3cc597d1b4454f0442c31e10a0a59bda1b8b9f18
Maksim909 <makops393@gmail.com>
2022-12-21 23:27:22 +0300

    добавить 1,3,js

---
+++ 1.3.js
@@ -0,0 +1,30 @@
+function getLength(arr){
+    console.log(arr)
+    //return length of arr
+    return arr.length
+  }
+  function getFirst(arr){
+    console.log(arr[0])
+    //return the first element of arr
+    return arr[0]
+  }
+  function getLast(arr){
+    console.log(arr[2])
+    //return the last element of arr
+    return arr[2]
+  }
+  function pushElement(arr){
+     var el=1
+    arr.push(el)
+    console.log(arr)
+    //push el to arr
+
+    return arr
+  }
+  function popElement(arr){
+    arr.pop()
+    console.log(arr)
+    //pop an element from arr
+
+    return arr
+  }

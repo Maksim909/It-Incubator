56db8d80e54dec105550c052ba5f7d27e7f2f87d
Maksim909 <makops393@gmail.com>
2023-03-23 15:44:12 +0300

    3.7.js

---
+++ 3.7.js
@@ -0,0 +1,6 @@
+const myList = [];
+myList[0] = ["Chocolate Bar", 15];
+myList[1] = ["Chocolate Bar", 16];
+myList[2] = ["Chocolate Bar", 17];
+myList[3] = ["Chocolate Bar", 18];
+myList[4] = ["Chocolate Bar", 19];

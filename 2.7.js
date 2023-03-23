dfd38cd5d09dd330e4c126a46b0c18ee9b3cb55a
Maksim909 <makops393@gmail.com>
2023-03-23 15:02:12 +0300

    2.7.js

---
+++ 2.7.js
@@ -0,0 +1,6 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "My " + myAdjective + " hot " + myNoun + " laughed " + myAdverb + " ourselves " + myVerb + "." ;
